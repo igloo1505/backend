abd7aaf42dcd2347b7e262ce4aa2dbb1b21673e3
Andrew <iglooworks1505@gmail.com>
2020-09-23 12:59:29 -0500

    initial commit. just beginning the medication timer server

---
+++ config/db.js
@@ -0,0 +1,16 @@
+const mongoose = require("mongoose");
+const config = require("config");
+const db = config.get("mongoURI");
+
+const connectDB = () => {
+  mongoose
+    .connect(db, {
+      useNewUrlParser: true,
+      useCreateIndex: false,
+      autoIndex: false,
+      useFindAndModify: false,
+      useUnifiedTopology: true,
+    })
+    .then(() => console.log("Medication Database Connected"));
+};
+module.exports = connectDB;
---
+++ seeder.js
@@ -0,0 +1,20 @@
+const users = [
+  {
+    name: "Andrew Iglinski",
+    email: "aiglinski@icloud.com",
+    password: "SomePassword123",
+    age: 31,
+  },
+  {
+    name: "Meaghan",
+    email: "SuperAwesome@icloud.com",
+    password: "SomePassword123",
+    age: 30,
+  },
+  {
+    name: "Tyler Durden",
+    email: "KindaAwesome@icloud.com",
+    password: "SomePassword123",
+    age: 35,
+  },
+];
